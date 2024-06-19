2b4a040923357e19227d7a1e0202c775304963f2
Pepe <josgarci@debian.debian.com>
2024-06-19 23:08:08 +0200

    script de configuración de wordpress pendiente de testear

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ ! -f /var/www/html/wp-config.php ]]
+then
+    # Descargar WordPress
+    wp core download --path=/var/www/html --locale=es_ES --allow-root
+
+    # Crear archivo de configuración wp-config.php
+    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+
+    # Cambiar permisos del archivo wp-config.php
+    chmod 644 /var/www/html/wp-config.php
+
+    # Instalar WordPress
+    wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_name=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_MAIL --allow-root
+
+    # Crear un usuario adicional
+    wp user create $WORDPRESS_USER $WORDPRESS_USER_MAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=editor --allow-root
+
+    # Crear el directorio de subidas y cambiar sus permisos
+    mkdir -p /var/www/html/wp-content/uploads
+    chown -R www-data:www-data /var/www/html/wp-content/uploads/*
+    chmod 775 /var/www/html/wp-content/uploads
+fi
+
+exec php-fpm7.3 -F
