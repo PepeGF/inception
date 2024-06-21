2f37df04106d2d2c0016bc489fc0df78cb1b0c06
Pepe <josgarci@debian.debian.com>
2024-06-22 00:02:34 +0200

    errores en la configuración de wordpress solucionados. Se abre con todo OK. Pendiente pruebas profundas

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -5,17 +5,26 @@ then
     # Descargar WordPress
     wp core download --path=/var/www/html --locale=es_ES --allow-root
 
+    echo "core download:" $? >> outputs
+
     # Crear archivo de configuración wp-config.php
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+
+    echo "config create: " $? >> outputs
 
     # Cambiar permisos del archivo wp-config.php
     chmod 644 /var/www/html/wp-config.php
 
-    # Instalar WordPress
-    wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_name=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_MAIL --allow-root
+    echo "chmod: " $? >> outputs
 
+    # Instalar WordPress
+    wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_name=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD --admin_email=$WORDPRESS_ADMIN_MAIL --allow-root
+
+    echo "core install: " $? >> outputs
     # Crear un usuario adicional
-    wp user create $WORDPRESS_USER $WORDPRESS_USER_MAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=editor --allow-root
+    wp user create $WORDPRESS_DB_USER $WORDPRESS_USER_MAIL --user_pass=$WORDPRESS_DB_PASSWORD --role=editor --allow-root
+    
+    echo "user create: " $? >> outputs
 
     # Crear el directorio de subidas y cambiar sus permisos
     mkdir -p /var/www/html/wp-content/uploads
